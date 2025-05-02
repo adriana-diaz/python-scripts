0cf0dd61acb65519edf0f91dbbd2343fae6aa23e
Adriana Diaz <122817007+adriana-diaz@users.noreply.github.com>
2025-05-02 08:56:21 -0600

    Update PCEP-30-0x.py
    loop y while

--- PCEP-30-0x.py
+++ PCEP-30-0x.py
@@ -180,3 +180,15 @@ for i in range(1,6):
     
 print("Ready or not, here I come!")
 ----------------------------------------------------
+# Create a for loop that counts from 0 to 10, and prints odd numbers to the screen
+for i in range(0, 11):
+    if i % 2 != 0:
+        print(i)
+----------------------------------------------------
+# Create a while loop that counts from 0 to 10, and prints odd numbers to the screen.
+x = 1
+while x < 11:
+    if x % 2 != 0:
+        print(x)
+    x += 1
+----------------------------------------------------
