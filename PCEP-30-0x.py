b26405dd27cbbf50a04bc95e01b4e27baac5aeeb
Adriana Diaz <122817007+adriana-diaz@users.noreply.github.com>
2025-05-02 07:28:53 -0600

    Update PCEP-30-0x.py

--- PCEP-30-0x.py
+++ PCEP-30-0x.py
@@ -149,3 +149,25 @@ else:
         print("Common year")
     else:
         print("Leap year")
+----------------------------------------------------
+# Programa para adivinar un numero secreto
+secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+guess = int(input("So, what is the secret number?"))
+
+while guess != secret_number:
+    print("Ha ha! You're stuck in my loop!")
+    guess=int(input("Try again:"))
+    
+print("Well done, muggle! You are free now.")
